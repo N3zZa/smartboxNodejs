e577caaf4138d19b28712e8b7a2e2af7fb38632c
nazar <nazar.developer007@gmail.com>
2023-03-27 21:26:21 +0300

    waf

--- index.js
+++ index.js
@@ -301,8 +301,8 @@ app.get(
   }
 );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
 
-// rm-rf xyz - удалить репозиторий с амазон
+// rm -rf xyz - удалить репозиторий с амазон
