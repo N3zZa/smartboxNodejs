1534e994dbf18621c259f35df3acb3a0453da08d
nazar <nazar.developer007@gmail.com>
2023-03-27 13:41:48 +0300

    waf

--- index.js
+++ index.js
@@ -17,13 +17,17 @@ let API_URL = `https://bazon.cc/api/json?token=${API_KEY}&type=film&page=2&cat=�
  const showMovies = async () => {
    try {
     const commits = await fetchData;
-      let items = commits.results.map((element) => 
-      `<div nv-el class='movieitem'>
-        <img src='${element.info.poster}' alt='imglogo' />
-        <h4>${element.info.rus}</h4>
-        <p>${element.info.year}</p>
-      </div>`
-    );
+      let items = commits.results.map(
+        (element) =>
+          `
+          <iframe nv-el-current onload="this.width=(screen.width - 200);this.height=(screen.height - 200);" id="${element.kinopoisk_id}" style="display:none; position:absolute; left: 0; top:0;" src="${element.link}" frameborder="0"></iframe>
+          <div onclick="var video = document.getElementById('${element.kinopoisk_id}'); video.style.display = 'block'; video[0].contentWindow.document.body.focus()" nv-el class='movieitem'>
+          <img src='${element.info.poster}' alt='imglogo' />
+          <h4>${element.info.rus}</h4>
+          <p>${element.info.year}</p>
+          </div>
+      `
+      );
       return items;
    } catch (error) {
      console.error(error);
@@ -34,6 +38,8 @@ let API_URL = `https://bazon.cc/api/json?token=${API_KEY}&type=film&page=2&cat=�
    try {
      const movies = await showMovies();
      const moviesItems = movies.join('')
+
+     const commits = await fetchData;
      // используем movies в шаблонной строке:
       const message = `<!DOCTYPE html>
 <html lang="en">
@@ -102,10 +108,11 @@ h1 {
 }
 
 .movies {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    display: flex;
+    flex-wrap: wrap;
     gap: 20px;
     margin-top: 30px;
+    width: 100%;
 }
 
 
@@ -224,6 +231,29 @@ h1 {
     ${moviesItems}
     </div>
     </div>
+    <script type='text/javascript'>
+    var arrowback = document.getElementById('arrowback')
+    var imglogo = document.getElementById('imglogo')
+    var categories = document.getElementById('categories')
+    var categorylist = document.getElementById('categorylist')
+
+    arrowback.addEventListener('nv-enter', function (event) {
+            window.history.go(-1)
+    });
+
+    imglogo.addEventListener('nv-enter', function (event) {
+            window.location='/'
+    });
+
+    categories.addEventListener('nv-enter', function (event) {
+            if (categorylist.style.display = 'none') {
+                categorylist.style.display = 'flex'
+            } else if (categorylist.style.display = 'flex') {
+                categorylist.style.display = 'none'
+            }
+    });
+
+    </script>
     <script type="text/javascript" src="../navigation/navigation.js"></script>
     <script type="text/javascript" src="../navigation/navigation.min.js"></script>
 </body>
