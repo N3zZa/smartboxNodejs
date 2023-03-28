5e15c718ed14730bfb5b92ffc67b546588ead8a1
nazar <nazar.developer007@gmail.com>
2023-03-28 14:10:15 +0300

    waf

--- index.js
+++ index.js
@@ -316,6 +316,11 @@ h1 {
             }
     });
 
+    window.onkeydown = evt => {
+    if (evt.key === 'Tab') {
+        evt.preventDefault();
+    }
+}
     </script>
     <script type="text/javascript" src="../navigation/navigation.js"></script>
     <script type="text/javascript" src="../navigation/navigation.min.js"></script>
