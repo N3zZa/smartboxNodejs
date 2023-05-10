a255c28826a393359290489b5402f775bea21533
Erlanoff <erlanoff.dev@gmail.com>
2023-05-10 19:57:28 +0600

    Мой коммит

---
+++ js/anime/animeSerialSeasons.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict"
+
+    window.App.animeSerialSeasons = [
+      {
+                     season: '1',
+                     episode: '0',
+                     id: '817855',
+                  },
+          
+    ] 
+  })();
+  
