2dade88eda94b9efe9e704d68535ed484609a59e
nazar <nazar.developer007@gmail.com>
2023-06-04 12:16:48 +0300

    много чего сделал поиск полностью готов

---
+++ js/search/searchVideosRender.js
@@ -0,0 +1,51 @@
+(function () {
+  var _inited;
+    _.templateSettings.interpolate = /\{\{([\s\S]+?)\}\}/g;
+  var itemHtml = _.template('<div data-content="filmInfo" data-film="{{filmPageId}}" data-id="{{id}}" style="background: url({{imgurl}}); background-repeat:no-repeat;  background-size:cover;" class="movieitem navigation-item nav-item" data-type="{{type}}"><h4 class="mainMovieTitle">{{title}}</h4></div>');
+    
+  window.App.scenes.video = {
+    init: function () {
+      this.$el = $(".js-scene-video");
+
+      this.$el.on("click", ".movieitem", this.onItemClick)
+
+      this.renderItems(App.searchedVideos);
+      _inited = true;
+    },
+
+    onItemClick: function (e) {
+        var filmPage = e.currentTarget.getAttribute("data-film");
+        var scene = e.currentTarget.getAttribute("data-content");
+        var item = "#" + filmPage;
+        $(".header").hide();
+        window.App.showContent(scene);
+        $(".filmInfoPage").hide();
+        $(item).show();
+    },
+
+    show: function () {
+      if (!_inited) {
+        this.init();
+      }
+
+      this.$el.show();
+    },
+
+    hide: function () {
+      this.$el.hide();
+    },
+
+    // showing items from videos.js
+    renderItems: function (items) {
+      var html = "";
+
+      console.log(items, itemHtml.toString())
+      for (var i = 0, len = items.length; i < len; i++) {
+        html += itemHtml(items[i]);
+      }
+
+      this.$el.empty().html(html);
+    },
+  };
+})();
+    
